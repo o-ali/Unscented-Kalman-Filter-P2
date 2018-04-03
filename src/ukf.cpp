b6edc3445677951d5ab7764e7000c10bf1dc1962
o-ali <omarwwali@gmail.com>
2018-04-02 22:17:41 -0400

    fix crash
    re ordered initialization variables to avoid crash

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -55,8 +55,6 @@ UKF::UKF() {
   Hint: one or more values initialized above might be wildly off...
   */
   is_initialized_ = false;
-  ///* Weights of sigma points
-  weights_ = VectorXd(2 * n_aug_ + 1);
 
   ///* State dimension
   n_x_ = 5;
@@ -74,6 +72,10 @@ UKF::UKF() {
   NIS_laser_ = 0.0;
   NIS_radar_ = 0.0;
   
+  ///* Weights of sigma points
+  weights_ = VectorXd(2 * 7 + 1);
+  
+  //variable used to display the the timestep with corresponding NIS calculation, not required for project
   counter = 1;
 }
 
@@ -306,14 +308,12 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
     Zsig(0, i) = p_x;
     Zsig(1, i) = p_y;
   }
-  
   //create vector for mean predicted measurement
   VectorXd z_pred = VectorXd(n_z);
   z_pred.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {
     z_pred = z_pred + weights_(i) * Zsig.col(i);
-  }
-  
+  } 
   //create matrix for predicted measurement covariance
   MatrixXd S = MatrixXd(n_z,n_z);
   S.fill(0.0);
@@ -321,7 +321,6 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
     VectorXd z_diff = Zsig.col(i) - z_pred;
     S = S + weights_(i) * z_diff * z_diff.transpose();
   }
-  
   //create matrix for cross correlation Tc
   MatrixXd Tc = MatrixXd(n_x_, n_z);
 
@@ -329,23 +328,13 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
   MatrixXd noise = MatrixXd(n_z,n_z);
   noise << std_laspx_*std_laspx_,0,0,std_laspy_*std_laspy_;
   S += noise;
-  
     //calculate cross correlation matrix
   Tc.fill(0.0);
   for (int i = 0; i < 2 * n_aug_ + 1; i++) {  //2n+1 simga points
-
     //residual
     VectorXd z_diff = Zsig.col(i) - z_pred;
-    //angle normalization not in lidar
-    //while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
-    //while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
-
     // state difference
     VectorXd x_diff = Xsig_pred_.col(i) - x_;
-    //angle normalization not in lidar
-    //while (x_diff(3)> M_PI) x_diff(3)-=2.*M_PI;
-    //while (x_diff(3)<-M_PI) x_diff(3)+=2.*M_PI;
-
     Tc = Tc + weights_(i) * x_diff * z_diff.transpose();
   }
 
@@ -354,10 +343,6 @@ void UKF::UpdateLidar(MeasurementPackage meas_package) {
 
   //residual
   VectorXd z_diff = z - z_pred;
-
-  //angle normalization not in lidar
-  //while (z_diff(1)> M_PI) z_diff(1)-=2.*M_PI;
-  //while (z_diff(1)<-M_PI) z_diff(1)+=2.*M_PI;
 
   //update state mean and covariance matrix
   x_ = x_ + K * z_diff;
